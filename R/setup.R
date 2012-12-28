ea6652ea2480d082fc2b973b826ea973e07f9bc6
Jeffrey Wong <jeff.ct.wong@gmail.com>
2012-12-28 00:00:00 +0000

    version 1.1

---
+++ R/setup.R
@@ -0,0 +1,19 @@
+setClass("pdist", representation(dist = "numeric",
+                                 n = "numeric",
+                                 p = "numeric"),
+         S3methods = T)
+
+#' extract parts of pdist
+#'
+#' @name [
+#' @aliases [,pdist-method
+#' @docType methods
+#' @rdname extract-methods
+setMethod("[", "pdist", function(x, i, j, ...) {
+  if (missing(j)) j = 1:x@p
+  x@dist[(i - 1) * x@p + j]
+})
+
+#' @method as.matrix pdist
+#' @S3method as.matrix pdist
+as.matrix.pdist = function(x, ...) matrix(x@dist, x@n, x@p, byrow=T)
--- src/pdist.c
+++ src/pdist.c
@@ -6,13 +6,24 @@ float dist(float* x, float *y, int nx, int ny, int p)
   float distance = 0;
   for(int i = 0; i < p; i++)
   {
-    float diff = x[i*nx] - y[i*ny];
+    //i*nx is the ith feature of the nxth row
+    float xi = x[i*nx];
+    float yi = y[i*ny];
+    if (isnan(xi) || isnan(yi)) {
+        continue;
+    }
+    float diff = xi - yi;
     distance += (diff*diff);
   }
   return sqrt(distance);
 }
 
 //C implementation of pdist
+//X is a matrix represented as a vector.  Columns of X are features
+//Y is a matrix represented as a vector.  Columns of Y are features
+//Rnx is the number of observations (rows) in X
+//Rny is the number of observations (rows) in Y
+//Rp is the length of the vectors in X and Y
 void Rpdist(float* X, float* Y, int* Rnx, int* Rny, int* Rp, float* distances)
 {
   int nx = Rnx[0];
@@ -20,7 +31,7 @@ void Rpdist(float* X, float* Y, int* Rnx, int* Rny, int* Rp, float* distances)
   int p = Rp[0];
   for(int i = 0; i < nx; i++) 
   {
-    float* x = X + i;  //a single row of X
+    float* x = X + i;  //a pointer to the beginning of the ith row of X
     for(int j = 0; j < ny; j++)
     {
       float* y = Y + j;
